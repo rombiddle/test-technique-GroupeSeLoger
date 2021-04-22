2108eb2d990a03c044b9c2467439d0260373501a
Romain <rbrunie@cityway.fr>
2021-04-22 10:30:54 +0200

    Create property listing model

---
+++ SeLoger/SeLoger/PropertyListing.swift
@@ -0,0 +1,20 @@
+//
+//  PropertyListing.swift
+//  SeLoger
+//
+//  Created by Romain Brunie on 22/04/2021.
+//
+
+import Foundation
+
+struct PropertyListing {
+    let bedrooms: Int
+    let city: String
+    let id: Int
+    let area: Float
+    let url: URL
+    let price: Float
+    let professional: String
+    let propertyType: String
+    let rooms: Int
+}
