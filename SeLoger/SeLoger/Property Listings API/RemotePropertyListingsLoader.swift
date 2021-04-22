5f17c32e29e118afe1fc5422bd12a97cc2a724f9
Romain <rbrunie@cityway.fr>
2021-04-22 16:33:08 +0200

    Use explicit JSON data for non 200 HTTP response tests

--- SeLoger/SeLoger/Property Listings API/RemotePropertyListingsLoader.swift
+++ SeLoger/SeLoger/Property Listings API/RemotePropertyListingsLoader.swift
@@ -32,8 +32,8 @@ public class RemotePropertyListingsLoader {
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case let .success((data, _)):
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
+            case let .success((data, response)):
+                if response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) {
                     if let root = try? JSONDecoder().decode(Root.self, from: data) {
                         completion(.success(root.items.toModels()))
                     }
