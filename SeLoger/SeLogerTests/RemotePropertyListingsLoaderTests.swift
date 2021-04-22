c801d4e9064303c080174cfe1e6d7a74073174d3
Romain <rbrunie@cityway.fr>
2021-04-22 10:48:51 +0200

    add test to load property listings

---
+++ SeLoger/SeLogerTests/RemotePropertyListingsLoaderTests.swift
@@ -0,0 +1,63 @@
+//
+//  SeLogerTests.swift
+//  SeLogerTests
+//
+//  Created by Romain Brunie on 21/04/2021.
+//
+
+import XCTest
+@testable import SeLoger
+
+class RemotePropertyListingsLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class RemotePropertyListingsLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
+        let (_, client) = makeSUT(url: url)
+                
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_init_RequestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+                
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemotePropertyListingsLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemotePropertyListingsLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+
+}
