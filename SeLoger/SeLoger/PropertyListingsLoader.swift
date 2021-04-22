029c692b22fea06d71f7b90ddfd5df2f36c4663d
Romain <rbrunie@cityway.fr>
2021-04-22 10:37:48 +0200

    Create the PropertyListingsLoader protocol boundary

---
+++ SeLoger/SeLoger/PropertyListingsLoader.swift
@@ -0,0 +1,14 @@
+//
+//  PropertyListingsLoader.swift
+//  SeLoger
+//
+//  Created by Romain Brunie on 22/04/2021.
+//
+
+import Foundation
+
+protocol PropertyListingsLoader {
+    typealias Result = Swift.Result<[PropertyListing], Error>
+    
+    func load(completion: @escaping (Result) -> Void)
+}
