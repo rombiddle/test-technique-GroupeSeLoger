44bbf63cd0d57bd5946548e1a73c741df4df9edd
Romain <rbrunie@cityway.fr>
2021-04-22 11:15:24 +0200

    Delivers invalidData on non 200 HTTP Response

--- SeLoger/SeLoger/Property Listings API/RemotePropertyListingsLoader.swift
+++ SeLoger/SeLoger/Property Listings API/RemotePropertyListingsLoader.swift
@@ -8,19 +8,31 @@
 import Foundation
 
 protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 class RemotePropertyListingsLoader {
     let client: HTTPClient
     let url: URL
     
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    func load() {
-        client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void) {
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
+            
+        }
     }
 }
--- SeLoger/SeLogerTests/RemotePropertyListingsLoaderTests.swift
+++ SeLoger/SeLogerTests/RemotePropertyListingsLoaderTests.swift
@@ -21,8 +21,8 @@ class RemotePropertyListingsLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-                
+        sut.load { _ in }
+
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
@@ -30,10 +30,36 @@ class RemotePropertyListingsLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
-                
+        sut.load { _ in }
+        sut.load { _ in }
+
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemotePropertyListingsLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complete(with: clientError)
+        
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+    
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        let samples = [199, 201, 300, 400, 500]
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemotePropertyListingsLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     // MARK: - Helpers
@@ -45,10 +71,26 @@ class RemotePropertyListingsLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
+        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+                
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
         
-        func get(from url: URL) {
-            requestedURLs.append(url)
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+            messages.append((url, completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index],
+                                           statusCode: code,
+                                           httpVersion: nil,
+                                           headerFields: nil)
+            messages[index].completion(nil, response)
         }
     }
 
