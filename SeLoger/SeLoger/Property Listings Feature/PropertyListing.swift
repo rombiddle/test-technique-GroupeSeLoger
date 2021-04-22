71e56b25e2cf0d9bf1d3752f7dc18805176c2104
Romain <rbrunie@cityway.fr>
2021-04-22 18:19:25 +0200

    Move URLSessionHTTPClient to its own file + remove @testable import since "SeLoger" framework should be used by other modules

--- SeLoger/SeLoger/Property Listings Feature/PropertyListing.swift
+++ SeLoger/SeLoger/Property Listings Feature/PropertyListing.swift
@@ -17,4 +17,16 @@ public struct PropertyListing: Equatable {
     public let professional: String
     public let propertyType: String
     public let rooms: Int
+    
+    public init(bedrooms: Int?, city: String, id: Int, area: Float, url: URL?, price: Float, professional: String, propertyType: String, rooms: Int) {
+        self.bedrooms = bedrooms
+        self.city = city
+        self.id = id
+        self.area = area
+        self.url = url
+        self.price = price
+        self.professional = professional
+        self.propertyType = propertyType
+        self.rooms = rooms
+    }
 }
--- SeLoger/SeLogerTests/URLSessionHTTPClientTests.swift
+++ SeLoger/SeLogerTests/URLSessionHTTPClientTests.swift
@@ -6,29 +6,7 @@
 //
 
 import XCTest
-@testable import SeLoger
-
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    private struct UnexpectedValuesRepresentation: Error {}
-        
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
+import SeLoger
 
 class URLSessionHTTPClientTests: XCTestCase {
     
