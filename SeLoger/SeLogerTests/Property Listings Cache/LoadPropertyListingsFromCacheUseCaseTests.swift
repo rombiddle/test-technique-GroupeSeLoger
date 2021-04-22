9498cbb23715ced7788aae58a9d1f4daf2ec6650
Romain <rbrunie@cityway.fr>
2021-04-22 22:58:22 +0200

    Does not delete cache upon `LocalPropertyListingsLoader` creation

--- SeLoger/SeLogerTests/Property Listings Cache/LoadPropertyListingsFromCacheUseCaseTests.swift
+++ SeLoger/SeLogerTests/Property Listings Cache/LoadPropertyListingsFromCacheUseCaseTests.swift
@@ -7,26 +7,25 @@
 
 import XCTest
 
+class LocalPropertyListingsLoader {
+    let store: PropertyListingsStore
+    
+    init(store: PropertyListingsStore) {
+        self.store = store
+    }
+}
+
+class PropertyListingsStore {
+    var deleteCachedPropertyListingsCallCount: Int = 0
+}
+
 class LoadPropertyListingsFromCacheUseCaseTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = PropertyListingsStore()
+        _ = LocalPropertyListingsLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedPropertyListingsCallCount, 0)
     }
 
 }
