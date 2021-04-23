a9c988f0c3157bb65230c4017fe521a8ec552317
Romain <rbrunie@cityway.fr>
2021-04-24 00:28:58 +0200

    Extract duplicate test code into reusable helper methods

--- SeLoger/SeLogerTests/Property Listings Cache/RealmPropertyListingsStoreTests.swift
+++ SeLoger/SeLogerTests/Property Listings Cache/RealmPropertyListingsStoreTests.swift
@@ -13,67 +13,34 @@ class RealmPropertyListingsStoreTests: XCTestCase {
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache retrieval")
         
-        sut.retrieve { result in
-            switch result {
-            case .success:
-                break
-                
-            default:
-                XCTFail("Expected empty result, got \(result) instead")
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toRetrieve: .success([]))
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache retrieval")
         
-        sut.retrieve { firstResult in
-            sut.retrieve { secondResult in
-                switch (firstResult, secondResult) {
-                case (.success, .success):
-                    break
-                    
-                default:
-                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(firstResult) instead")
-                }
-                
-                exp.fulfill()
-            }
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toRetrieveTwice: .success([]))
     }
 
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
         let sut = makeSUT()
         let listings = uniqueItems().locals
-        let exp = expectation(description: "Wait for cache retrieval")
         
-        sut.insert(listings) { insertionError in
-            XCTAssertNil(insertionError, "Expected property listings to be inserted successfully")
-            
-            sut.retrieve { retrieveResult in
-                switch retrieveResult {
-                case let .success(retrievedListings):
-                    XCTAssertEqual(retrievedListings, listings)
-                    
-                default:
-                    XCTFail("Expected found result with property listings \(listings), got \(retrieveResult) instead")
-                }
-                
-                exp.fulfill()
-            }
-        }
+        insert(listings, to: sut)
         
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toRetrieve: .success(listings))
     }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+        let listings = uniqueItems().locals
+        
+        insert(listings, to: sut)
+        
+        expect(sut, toRetrieveTwice: .success(listings))
+    }
+
 
     // - MARK: Helpers
 
@@ -85,6 +52,40 @@ class RealmPropertyListingsStoreTests: XCTestCase {
     
     private func testRealmInMemoryConfiguration() -> Realm.Configuration {
         Realm.Configuration(inMemoryIdentifier: "\(type(of: self))Realm")
+    }
+    
+    private func insert(_ listings: [LocalPropertyListing], to sut: RealmPropertyListingsStore) {
+        let exp = expectation(description: "Wait for cache insertion")
+        
+        sut.insert(listings) { insertionError in
+            XCTAssertNil(insertionError, "Expected property listng to be inserted successfully")
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func expect(_ sut: RealmPropertyListingsStore, toRetrieve expectedResult: PropertyListingsStore.RetrievalResult, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { retrievedResult in
+            switch (expectedResult, retrievedResult) {
+            case let (.success(expected), .success(retrieved)):
+                XCTAssertEqual(expected, retrieved)
+                
+            default:
+                XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func expect(_ sut: RealmPropertyListingsStore, toRetrieveTwice expectedResult: PropertyListingsStore.RetrievalResult, file: StaticString = #filePath, line: UInt = #line) {
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
     }
 
 }
