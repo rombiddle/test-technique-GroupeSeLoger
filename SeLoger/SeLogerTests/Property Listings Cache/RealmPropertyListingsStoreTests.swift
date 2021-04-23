25a0d225e81721b5d5bbdf97a594aa2f816d0ecb
Romain <rbrunie@cityway.fr>
2021-04-23 18:20:22 +0200

    Create Realm store - Retrieving from empty cache delivers empty result

---
+++ SeLoger/SeLogerTests/Property Listings Cache/RealmPropertyListingsStoreTests.swift
@@ -0,0 +1,38 @@
+//
+//  RealmPropertyListingsStoreTests.swift
+//  SeLogerTests
+//
+//  Created by Romain Brunie on 23/04/2021.
+//
+
+import XCTest
+import SeLoger
+
+class RealmPropertyListingsStore {
+    func retrieve(completion: @escaping PropertyListingsStore.RetrievalCompletion) {
+        completion(.success([]))
+    }
+}
+
+class RealmPropertyListingsStoreTests: XCTestCase {
+
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = RealmPropertyListingsStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { result in
+            switch result {
+            case .success:
+                break
+                
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+
+}
