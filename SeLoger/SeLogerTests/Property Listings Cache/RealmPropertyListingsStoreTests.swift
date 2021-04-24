332db8384b026bec7113fbdc7aec6a8de0a45abe
Romain <rbrunie@cityway.fr>
2021-04-24 10:32:11 +0200

    Proved that the `RealmPropertyListingsStore` side-effects run serially

--- SeLoger/SeLogerTests/Property Listings Cache/RealmPropertyListingsStoreTests.swift
+++ SeLoger/SeLogerTests/Property Listings Cache/RealmPropertyListingsStoreTests.swift
@@ -54,6 +54,32 @@ class RealmPropertyListingsStoreTests: XCTestCase {
 
         expect(sut, toRetrieve: .success([]))
     }
+    
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        var completedOperationsInOrder = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueItems().locals) { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCachedPropertyListings { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueItems().locals) { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
+    }
 
     // - MARK: Helpers
 
